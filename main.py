2460f49f266e9f44de427bec3edfbd3ef0645edc
letuwwa <letuwwa@gmail.com>
2021-11-04 11:29:41 +0300

    test: user info

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message): await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler(commands=['user'])
+async def send_user(message: types.Message):
+    await message.reply(f"{message.from_user.username}, {message.from_user.id}, {message.text}")
 
 
 @dp.message_handler()
-async def echo(message: types.Message): await message.answer(message.text)
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 
 if __name__ == '__main__':
